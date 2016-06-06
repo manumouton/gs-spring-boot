f676e782578e3472415f3aba54a21223fe09660b
Manu Mouton <manumouton@gmail.com>
2016-06-06 10:23:44 +0200

    Modifying greetings

--- src/main/java/hello/HelloController.java
+++ src/main/java/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from Spring Boot and now going through an automated build in Jenkins !";
+        return "Another greetings from Spring Boot and now going through an automated build in Jenkins !";
     }
     
 }
