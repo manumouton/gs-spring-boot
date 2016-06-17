88c21024cf819af5a2a8442cd225806bb36ca7a4
Manu Mouton <manumouton@gmail.com>
2016-06-17 08:37:43 +0200

    Test for Sonar

--- src/test/java/hello/GildedRoseTest.java
+++ src/test/java/hello/GildedRoseTest.java
@@ -11,10 +11,10 @@ public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("fixme", 0, 0) };
+        Item[] items = new Item[] { new Item("fixme2", 0, 0) };
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+        assertEquals("fixme2", app.items[0].name);
     }
 
 }
